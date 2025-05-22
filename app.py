253482ad002b326d872b38159c1948b5605c8ae0
Zafar Karmani <52927225+zafarkarmani@users.noreply.github.com>
2025-05-22 11:51:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 import streamlit as st
 import joblib
+import os
+import requests
 import numpy as np
 
-# Load vectorizer and model
-vectorizer = joblib.load("tfidf_vectorizer.pkl")
-model = joblib.load("voting_model.pkl")
+# Function to download large model from Hugging Face
+@st.cache_data
+def download_model():
+    url = "https://huggingface.co/taqijaved/voting-model/resolve/main/voting_model.pkl"
+    filename = "voting_model.pkl"
+    if not os.path.exists(filename):
+        with st.spinner("Downloading model..."):
+            response = requests.get(url)
+            with open(filename, "wb") as f:
+                f.write(response.content)
+    return filename
 
+# Download and load model
+model_path = download_model()
+model = joblib.load(model_path)
+
+# Load small tf-idf vectorizer from local
+vectorizer = joblib.load("tfidf_vectorizer.pkl")
+
+# Streamlit UI
 st.title("Sentiment Analysis App")
 text_input = st.text_area("Enter text to analyze sentiment:")
 
